e8e0d50ec38cad995cd822deb255faec43c37137
agentfreedom <agentfreedom@zoho.com>
2022-01-28 19:06:43 +0200

    init: start "brackets" task + feat: add a declaration and assignment "let stack" to an empty array, and "let brackets" to an expression that concatenates the "bracketsConfig" array into a join method string, passing the result to the replace method, which returns a new string after comparing it with the pattern "/, /g,"

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
 module.exports = function check(str, bracketsConfig) {
 
-    const brackets = {
-        ')': '(',
-        ']': '[',
-        '}': '{'
-    };
+    let brackets = bracketsConfig.join('').replace(/,/g, '');
+    let stack = [];
 
-    const s = [];
-
-    for (let i = 0; i < str.length; i++) {
-        if (isClosedBracket(str[i])) {
-            if (brackets[str[i]] !== s.pop()) return false;
-        } else {
-            s.push(str[i]);
-        }
-    }
-    return s.length === 0;
-}
-
-function isClosedBracket(ch) {
-    return [')', ']', '}'].indexOf(ch) > -1;
 }
